288d6b3544580275d89cc91a7c5e58543ad99e1d
Jakub Olcha <olo75jg12@gmail.com>
2024-05-24 20:38:50 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from './components'
+
 function App() {
   return (
     <div>
-      3D portfolio
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
