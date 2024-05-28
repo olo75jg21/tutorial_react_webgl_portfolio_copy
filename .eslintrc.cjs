adaf063f3ae79bc74ca1ed7f3936252ad527fb26
Jakub Olcha <olo75jg12@gmail.com>
2024-05-28 22:53:31 +0200

    mobile 3d

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
+    // 'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
-      'warn',
+      // 'warn',
       { allowConstantExport: true },
     ],
   },
